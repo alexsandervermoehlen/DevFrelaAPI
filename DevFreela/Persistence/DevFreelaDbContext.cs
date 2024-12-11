fad0737e7a349550a5374ca3e56f242b95930865
Alexsander Vermoehlen <Alexsander.Vermoehlen@unidavi.edu.br>
2024-12-11 08:03:05 -0300

    Inicio da criação da modelagem de dados

---
+++ DevFreela/Persistence/DevFreelaDbContext.cs
@@ -0,0 +1,39 @@
+﻿using DevFreela.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevFreela.Persistence;
+
+public class DevFreelaDbContext : DbContext
+{
+    public DevFreelaDbContext(DbContextOptions<DevFreelaDbContext> options)
+        : base(options)
+    {
+        
+    }
+    
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Skill> Skills { get; set; }
+    public DbSet<UserSkill> UserSkills { get; set; }
+    public DbSet<ProjectComment> ProjectComments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder
+            .Entity<Skill>(e =>
+            {
+                e.HasKey(s => s.Id);
+            });
+
+        builder
+            .Entity<UserSkill>(e =>
+            {
+                e.HasKey(us => us.Id);
+
+                e.HasOne(us => us.Skill)
+                    .WithMany();
+            });
+        
+        base.OnModelCreating(builder);
+    }
+}
